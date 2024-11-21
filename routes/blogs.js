cfd7944d9144dd5dcd95f2b4e3d30fc38ea05079
rhexa <akun.kripitit@gmail.com>
2024-11-21 11:44:59 +0200

    exercise 11

--- routes/blogs.js
+++ routes/blogs.js
@@ -29,12 +29,17 @@ router.put('/:id', async (req, res) => {
   res.json(blog);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', tokenExtractor, async (req, res) => {
   const id = req.params.id;
   const blog = await Blog.findByPk(id);
   if (!blog) {
     return res.status(404).json({ message: 'Blog not found' });
   }
+
+  if (blog.userId !== req.decodedToken.id) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+  
   await blog.destroy();
   res.status(204).end();
 });
